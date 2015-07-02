e5341fd7b9d48b3879948a63a6b3d8ed25ff0ad3
JaredGordon <jgordon@pivotal.io>
2015-07-02 14:56:28 -0400

    register with eureka.

--- src/main/java/org/springframework/nanotrader/quote/Application.java
+++ src/main/java/org/springframework/nanotrader/quote/Application.java
@@ -2,12 +2,14 @@ package org.springframework.nanotrader.quote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
