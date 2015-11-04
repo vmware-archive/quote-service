4aac5fa5cd67915e4e91ecd076b7512ba76af77f
JaredGordon <jgordon@pivotal.io>
2015-11-04 15:02:13 -0500

    eurika-ify.

---
+++ src/main/java/org/springframework/nanotrader/quote/CloudConfig.java
@@ -0,0 +1,12 @@
+package org.springframework.nanotrader.quote;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableEurekaClient
+@Profile("cloud")
+public class CloudConfig {
+
+}
