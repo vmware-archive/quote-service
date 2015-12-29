94d9ca72fc392b27f295ed51fc3215677ad37eb5
JaredGordon <jgordon@pivotal.io>
2015-12-29 13:46:12 -0500

    update test.

--- ci/test.sh
+++ ci/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-app_name=$(cat source/manifest.yml | grep "name:" | awk '{print $NF}')
+app_name=$(cat source/ci/manifest.yml | grep "name:" | awk '{print $NF}')
 
-curl -f "$app_name.$CF_APPS_DOMAIN"
+curl -f "$app_name.$CF_APPS_DOMAIN/quotes/GOOG"
 exit $?
